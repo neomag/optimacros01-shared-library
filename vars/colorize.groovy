828ad5efa6468bc03d51cb98e3e66818d33aa275
neomag <neomag@gmail.com>
2024-10-24 00:00:45 +0300

    fix

--- vars/colorize.groovy
+++ vars/colorize.groovy
@@ -10,8 +10,9 @@ def call(String input, String color) {
         throw new IllegalArgumentException("Unsupported color: $color. Use 'red', 'green', or 'blue'.")
     }
     
-    println "debug line"
-    println "${input} ${color}"
+    //println "debug line"
+    //println "${input} ${color}"
+    println "${colors[color]}${input}${colors['reset']}"
 
 
     return "${colors[color]}${input}${colors['reset']}"
